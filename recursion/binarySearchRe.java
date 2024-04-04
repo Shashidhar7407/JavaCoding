451922793ab474f4defa20dfa393e0d8541d75d4
Shashidhar7407 <shashank.papishetty@gmail.com>
2024-04-04 08:24:22 +0530

    latest code changes in binary

--- recursion/binarySearchRe.java
+++ recursion/binarySearchRe.java
@@ -2,8 +2,8 @@ package recursion;
 
 public class binarySearchRe {
     public static void main(String[] args) {
-        int [] arr={1,2,3,4,5,6,7,8,9};
-        int target=-10;
+        int [] arr={1,2,3,4,5,6,7,8,9,61,78};
+        int target=61;
         System.out.println(search(arr, target, 0, arr.length-1));
         
     }
