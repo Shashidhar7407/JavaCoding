399ef7313d9bb255b736ef06d4dfb2c5c7b284b9
Shashidhar7407 <shashank.papishetty@gmail.com>
2024-04-27 14:26:38 +0530

    latest code changes

--- basic java coding/palindromeNumber.java
+++ basic java coding/palindromeNumber.java
@@ -1,18 +1,19 @@
 public class palindromeNumber {
     public static void main(String[] args) {
-        int x = 12345; 
-        int remainder,sum=0,temp;
-        temp=x;
-        while(x>0){
-            remainder=x%10;
-            sum=(sum*10)+remainder;
-            x=x/10;
+        int x = 12345;
+        int remainder, sum = 0, temp;
+        temp = x;
+        while (x > 0) {
+            remainder = x % 10;
+            sum = (sum * 10) + remainder;
+            x = x / 10;
         }
-        if(temp==sum){
+        if (temp == sum) {
             System.out.println("the number is palindrome");
-        }
-        else{
+        } else {
             System.out.println("The Number is not a Palindrome");
+            System.out.println("shashi");
+
         }
     }
 }
